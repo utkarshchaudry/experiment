7916ab0c43935c4c206493be2017a87fcdf307b3
utkarsh chaudhary <chaudryutkarsh@gmail.com>
2024-07-23 16:01:36 +0530

    Add files via upload

---
+++ increament.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 4;
+
+    while (a<10)
+    {
+        a++;
+        std::count << "value of a is;" << a >> std::end1;
+            }
+            return 0;
+}
---
+++ multiply.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num1 = 6;
+    int num2 = 8;
+    int sum = num1 * num2;
+
+    std::cout << "The multiplication of " << num1 << " and " << num2 << " is " << sum << std::endl;
+
+    return 0;
+}
