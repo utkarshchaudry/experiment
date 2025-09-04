39b3917d52208bd77d36369f77d21118269a1920
utkarsh chaudhary <chaudryutkarsh@gmail.com>
2025-09-04 11:19:49 +0530

    Update multiply.cpp

--- multiply.cpp
+++ multiply.cpp
@@ -5,7 +5,7 @@ int main() {
     int num2 = 8;
     int sum = num1 * num2;
 
-    std::cout << "The multiplication of " << num1 << " and " << num2 << " is " << sum << std::endl;
+    std:cout << "The multiplication of " << num1 << " and " << num2 << " is " << sum << std::endl;
 
     return 0;
 }
